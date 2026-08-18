331d98c6da113f6032b40238f03e5e3d84f4d7fa
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-08-18 20:59:57 +0300

    Expose acquisition corpus and retrieval APIs

--- cardiatlas/__init__.py
+++ cardiatlas/__init__.py
@@ -1,10 +1,12 @@
 """Virelion CardiAtlas: structured cardiac biomedical knowledge."""
 
+from .acquisition import AcquisitionTarget, acquisition_plan, plan_as_dict
 from .api import AtlasAPI
 from .build import ReferenceBuild, build_reference, populate_registry
 from .catalog import CatalogGroup, attach_datasets, group_datasets
 from .claims import Claim, ClaimAssessment, ClaimStore
 from .contracts import AtlasContext, context_from_dict
+from .corpus import CorpusReport, corpus_report
 from .diff import SnapshotDiff, diff_records
 from .evidence import EvidenceScore, score_evidence
 from .export import write_cardiBench_candidates, write_records, write_release
@@ -23,6 +25,7 @@ from .query import Query, QueryHit, graph_context, query_registry
 from .registry import AtlasRegistry
 from .release import ReleaseManifest, create_manifest, digest_records, verify_digest
 from .release_checks import ReleaseCheck, ReleaseReadiness, assess_release
+from .retrieval import RetrievalResult, neighborhood_retrieve, retrieve
 from .sample_ingest import ingest_sample_rows, sample_from_metadata
 from .service import AtlasService
 from .sources import SourceResult, ingest_source
@@ -30,19 +33,20 @@ from .sqlite import SQLiteAtlasStore
 from .studies import StudyQC, assess_study, study_from_datasets
 
 __all__ = [
-    "AtlasAPI", "AtlasContext", "AtlasGraph", "AtlasRecord", "AtlasRegistry", "AtlasService", "BenchmarkCandidate",
-    "CatalogGroup", "CellStateRecord", "Claim", "ClaimAssessment", "ClaimStore", "Concept", "DatasetRecord",
-    "EvidenceRecord", "EvidenceScore", "HarmonizedValue", "IdentifierResolution", "InterventionRecord", "MarkerRecord",
-    "PhenotypeRecord", "ProvenanceBundle", "ProvenanceEvent", "Query", "QueryHit", "QualityReport", "Relation",
-    "ReferenceBuild", "ReleaseCheck", "ReleaseManifest", "ReleaseReadiness", "SampleRecord", "SnapshotDiff",
-    "SourceResult", "SQLiteAtlasStore", "StudyQC", "StudyRecord", "assess_dataset", "assess_release", "assess_study",
-    "attach_datasets", "audit_datasets", "benchmark_readiness", "build_reference", "canonical_concept_id", "canonical_key",
-    "concepts_by_category", "context_from_dict", "create_manifest", "dataset_to_benchmark_candidate", "descendants", "diff_records",
-    "digest_records", "group_datasets", "harmonize_condition", "harmonize_label", "harmonize_modality", "ingest_sample_rows",
-    "ingest_source", "load_into_registry", "migrate_payload", "migrate_records", "normalize_accession", "normalize_gene_symbol",
-    "normalize_label", "normalize_species", "populate_registry", "query_registry", "read_bundle", "record_from_dict",
-    "resolve_concept", "resolve_identifier", "sample_from_metadata", "score_evidence", "study_from_datasets", "verify_digest",
-    "write_cardiBench_candidates", "write_records", "write_release",
+    "AcquisitionTarget", "AtlasAPI", "AtlasContext", "AtlasGraph", "AtlasRecord", "AtlasRegistry", "AtlasService",
+    "BenchmarkCandidate", "CatalogGroup", "CellStateRecord", "Claim", "ClaimAssessment", "ClaimStore", "Concept",
+    "CorpusReport", "DatasetRecord", "EvidenceRecord", "EvidenceScore", "HarmonizedValue", "IdentifierResolution",
+    "InterventionRecord", "MarkerRecord", "PhenotypeRecord", "ProvenanceBundle", "ProvenanceEvent", "Query", "QueryHit",
+    "QualityReport", "ReferenceBuild", "Relation", "ReleaseCheck", "ReleaseManifest", "ReleaseReadiness", "RetrievalResult",
+    "SampleRecord", "SnapshotDiff", "SourceResult", "SQLiteAtlasStore", "StudyQC", "StudyRecord", "acquisition_plan",
+    "assess_dataset", "assess_release", "assess_study", "attach_datasets", "audit_datasets", "benchmark_readiness",
+    "build_reference", "canonical_concept_id", "canonical_key", "concepts_by_category", "context_from_dict", "corpus_report",
+    "create_manifest", "dataset_to_benchmark_candidate", "descendants", "diff_records", "digest_records", "graph_context",
+    "group_datasets", "harmonize_condition", "harmonize_label", "harmonize_modality", "ingest_sample_rows", "ingest_source",
+    "load_into_registry", "migrate_payload", "migrate_records", "namespace", "neighborhood_retrieve", "normalize_accession",
+    "normalize_gene_symbol", "normalize_label", "normalize_species", "plan_as_dict", "populate_registry", "query_registry",
+    "read_bundle", "record_from_dict", "resolve_concept", "resolve_identifier", "retrieve", "sample_from_metadata",
+    "score_evidence", "study_from_datasets", "verify_digest", "write_cardiBench_candidates", "write_records", "write_release",
 ]
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
